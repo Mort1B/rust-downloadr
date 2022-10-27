1e066df17e2db57e06095eea1230873a4a153c6b
Morten Bergseng <bergseng.m@gmail.com>
2022-10-27 17:03:23 +0200

    Update main.rs
    added lazy_static, improves benchmark by 67%

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ use base64::decode_config;
 use clap::Parser;
 use data::{Args, ChunkData, Root};
 use std::{error::Error, fs};
+use lazy_static::lazy_static;
 extern crate base64;
 
 mod data;
 
+lazy_static! {
+    static ref HTTP_CLIENT: reqwest::Client = reqwest::Client::new();
+}
 // There are two ways of doing this, either(1) by starting at the size/ length of the vector we are given from calling the
 // get_and_decode_chunk_data function and subtracting the length/size of each vector of bytes until we reach end_offset - size.
 // The other way(2) is by subtracting the size at the beginning and adding the length/size of each vector of bytes until we reach the
@@ -53,10 +57,8 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 }
 
 async fn get_tx_offset_data(tx: &str) -> Result<Root, Box<dyn Error + Send + Sync>> {
-    let response = reqwest::get(format!("https://arweave.net/tx/{}/offset", tx).as_str())
-        .await?
-        .text()
-        .await?;
+    let url = format!("https://arweave.net/tx/{}/offset", tx);
+    let response = HTTP_CLIENT.get(url.as_str()).send().await?.text().await?;
 
     let root = serde_json::from_str::<Root>(&response)?;
 
@@ -64,12 +66,11 @@ async fn get_tx_offset_data(tx: &str) -> Result<Root, Box<dyn Error + Send + Syn
 }
 
 async fn get_and_decode_chunk_data(offset: i64) -> Result<Vec<u8>, Box<dyn Error + Send + Sync>> {
-    let response = reqwest::get(format!("https://arweave.net/chunk/{}", offset).as_str())
-        .await?
-        .text()
-        .await?;
+    let url = format!("https://arweave.net/chunk/{}", offset);
+    let response = HTTP_CLIENT.get(url.as_str()).send().await?.text().await?;
 
     let chunkdata = serde_json::from_str::<ChunkData>(&response)?;
+
     let decoded = decode_config(chunkdata.chunk, base64::URL_SAFE)?;
 
     Ok(decoded)
